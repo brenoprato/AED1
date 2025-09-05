e70293e803422884103ddb3ab13461a807ee561c
brenodebian <brenoppprado@gmail.com>
2025-09-05 19:27:03 -0300

    add beecrowd excercises folder

--- beecrowd/menor-e-posicao.c
+++ beecrowd/menor-e-posicao.c
@@ -7,19 +7,21 @@ int main() {
     
     int X[N];
     
-    scanf("%d", X[0]);
+    scanf("%d", &X[0]);
     menor = X[0];
     
-    for (int i = 1; i < N; i++){
-        scanf("%d", X[i]);
-        if (menor > X[i]){
+    for (int i = 1; i < N; i++) {
+        scanf("%d", &X[i]);
+        if (menor > X[i]) {
             menor = X[i];
             pos = i;
         }
     }
     
-    printf("Menor Valor: %d \n", menor);
-    printf("Posicao: %d", pos);
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %d\n", pos);
  
     return 0;
 }
+
+
